016543dbd0db8c44eb62766c45d470645059fb4a
Andrea Massa <andreamassa1999@gmail.com>
2023-02-01 16:45:28 -0500

    Refractoring
    Cleaned up the code

--- index.js
+++ index.js
@@ -67,9 +67,14 @@ function searchShows(event){
     .then(function(response){
         //If the call works, parse the data
         response.json().then(function(data){
-            //once the data is parsed, process it by creating objects with the necessary information,
-            //then display it, these two functions handle this
-            displayShows(processData(data));
+            //Parse the data by creating shows object with the necessary information            
+            let shows = [];
+            for (let n in data){
+                let s = new Show(data[n].show);
+                shows.push(s)
+            }                                                        
+            //then display the shows on the html DOM.
+            displayShows(shows);
         }); 
     })
     //Handle error in case the API call does not work
@@ -82,9 +87,7 @@ function searchShows(event){
 
 /*THIS FUNCTION MAKES A API CALL TO TV MAZE IN ORDER TO GET DETAILED INFORMATION
 ABOUT A SHOW THAT WILL APPEAR WHENVER THE USER CLICKS ON A SHOW AFTER SEARCH */
-function searchShowDetail(show_id){
-    console.log('Show is clicked, the id is ' + show_id);
-    console.log('Making API Call to api.tvmaze.com/shows/' + show_id);
+function searchShowDetail(show_id){    
     let url = `https://api.tvmaze.com/shows/${show_id}`;
 
     //Make the API Call to the TV Maze to get detailed infromation about the show clicked
@@ -93,8 +96,7 @@ function searchShowDetail(show_id){
         console.log('Successful Api Call!');
         response.json()
         .then(function(data){
-           let show = new Show_Detailed(data);
-           console.log(show);
+           let show = new Show_Detailed(data);           
            displayShowDetails(show);
         })
         .catch(function(e){
@@ -107,39 +109,60 @@ function searchShowDetail(show_id){
 }
 
 
-
 /*
-THIS FUNCTION PROCESSES THE JSON DATA GIVEN IN ORDER TO CREATE OBJECTS 
-WITH THE INFORMATION WE NEED FOR EACH SHOW. EACH SHOW IS THEN PUSHED ON AN
-ARRAY.
- */
-function processData(data){
-    let shows = [];
-
-    for (let n in data){
-        s = new Show(data[n].show);
-        shows.push(s)
-    }    
-
-    return shows;
-}
-
-
-
-/*
-THIS FUNCTION DISPLAYS THE SEARCH RESULTS ON THE DOM 
+THIS FUNCTION DISPLAYS THE SEARCH RESULTS ON THE DOM ONCE THE DATA IS FETCHED   
  */
 function displayShows(shows){
+    /*
+    THIS FUNCTION RETURNS INDIVIDUAL DOM NODE FOR THE SHOW GIVEN
+    */
+    function createShowDomElement(show){
+
+        //Create the Div element that will contain the Show Data
+        let show_container = document.createElement('div');
+        show_container.classList.add('tv-show');
+
+        //Create the title heading
+        let show_title_heading = document.createElement('h3');
+        show_title_heading.appendChild(document.createTextNode(show.title));
+
+        //Create the image container and the image
+        let image_container_div = document.createElement('div');
+        image_container_div.classList.add('show-img_container');
+        let image_node = document.createElement('img');
+        image_node.classList.add('show-img');
+        image_node.setAttribute('src', show.image);
+        image_container_div.appendChild(image_node);
+
+        //Create the genres paragraph
+        let genres_paragraph = document.createElement('p');
+        genres_paragraph.appendChild(document.createTextNode(show.genres));
+        genres_paragraph.classList.add('show-genre');
+
+        //Create the description paragraph
+        let description_paragraph = document.createElement('p');
+        description_paragraph.innerHTML = (show.description);
+        description_paragraph.classList.add('show-description');
+        
+
+        //Put everything together
+        show_container.appendChild(show_title_heading);
+        show_container.appendChild(image_container_div);
+        show_container.appendChild(genres_paragraph);
+        show_container.appendChild(description_paragraph);
+        return show_container;
+    }
+
     //Clear Previous search
-    clearFormatting(false, true, true);
+    clearFormatting(true, true, true);
 
     //Turn result section visible
     document.getElementById('result_section').classList.remove('hidden');
 
-    //Create show elements on HTML
+    //For each show in the array create a DOM element and attach it to the result section
     for (let show in shows){
         let show_node = createShowDomElement(shows[show]);
-        //Attach the display show detail function to each show
+        //Attach the display show detail function to each show, so if user clicks on it, it'll be called
         show_node.addEventListener('click', function(){
             searchShowDetail(shows[show].id)
         });
@@ -152,8 +175,68 @@ function displayShows(shows){
 THIS FUNCTION DISPLAYS THE SHOW DETAIL ONCE THE USER CLICKS ON A SHOW
 */
 function displayShowDetails(detailed_show){
+    /*  
+    THIS FUNCTION CREATES THE DOM ELEMENT FOR A DETAILED SHOW 
+    WHICH WILL BE APPENDED INTO THE SHOW_EXPANDED_MODAL ELEMENT
+    */
+    function createDetailedShowDomElement(detailed_show){            
+        //Container
+        let d_show_container = document.createElement('div');
+        d_show_container.classList.add('detailed_show');
+
+        //Show title
+        let d_show_title_heading = document.createElement('h3');
+        d_show_title_heading.innerText = detailed_show.title;
+        d_show_title_heading.classList.add('d_show_title');
+
+        //Show review in stars
+        let stars_container = document.createElement('span');    
+            //Round the rating to the nearest integer
+            let rating = Math.round(detailed_show.rating);        
+            //Based on the rating return x number of stars        
+            let stars_text = '';
+            for (let x = 0; x < rating; x++){            
+                stars_text += '*'
+            }     
+        stars_container.appendChild(document.createTextNode(stars_text));    
+
+        //Show image
+        let d_show_image = document.createElement('img');
+        if(detailed_show.image != null){        
+            d_show_image.setAttribute('src', detailed_show.image);
+        }
+        else{
+            d_show_image.setAttribute('alt', 'No image available for this show');
+        }
+
+        //Show Description
+        let d_show_description = document.createElement('p');
+        d_show_description.innerHTML = detailed_show.description;
+
+        //Show Genres
+        let d_show_genres = document.createElement('p');
+        d_show_genres.appendChild(document.createTextNode(detailed_show.genres));
+
+        //Premiered and End dates
+        let d_show_pe = document.createElement('p');
+        d_show_pe.append(document.createTextNode(detailed_show.premiered_date), document.createTextNode(detailed_show.ended_date));
+
+        //Country
+        let d_show_country = document.createElement('p');
+        d_show_country.append(document.createTextNode(detailed_show.country));
+
+        //Official Website
+        let d_show_website = document.createElement('p');
+        d_show_website.append(document.createTextNode(detailed_show.official_site));
+
+        //Put Everything together
+        d_show_container.append(d_show_title_heading, stars_container, d_show_image, d_show_description, d_show_genres, d_show_pe, d_show_country, d_show_website);            
+        return (d_show_container);
+    }
+
      //Turn show expanded section visible
      document.getElementById('show_expanded_modal').classList.remove('hidden');
+     
      //Turn the background opaque
      document.getElementById('result_section').classList.add('opaque');
      
@@ -164,131 +247,20 @@ function displayShowDetails(detailed_show){
 
 
 /*
-THIS FUNCTION CREATES INDIVIDUAL DOM NODE ELEMENTS FOR EACH SHOW
-*/
-function createShowDomElement(show){
-
-    //Create the Div element that will contain the Show Data
-    let show_container = document.createElement('div');
-    show_container.classList.add('tv-show');
-
-    //Create the title heading
-    let show_title_heading = document.createElement('h3');
-    show_title_heading.appendChild(document.createTextNode(show.title));
-
-    //Create the image container and the image
-    let image_container_div = document.createElement('div');
-    image_container_div.classList.add('show-img_container');
-    let image_node = document.createElement('img');
-    image_node.classList.add('show-img');
-    image_node.setAttribute('src', show.image);
-    image_container_div.appendChild(image_node);
-
-    //Create the genres paragraph
-    let genres_paragraph = document.createElement('p');
-    genres_paragraph.appendChild(document.createTextNode(show.genres));
-    genres_paragraph.classList.add('show-genre');
-
-    //Create the description paragraph
-    let description_paragraph = document.createElement('p');
-    description_paragraph.innerHTML = (show.description);
-    description_paragraph.classList.add('show-description');
-    
-
-    //Put everything together
-    show_container.appendChild(show_title_heading);
-    show_container.appendChild(image_container_div);
-    show_container.appendChild(genres_paragraph);
-    show_container.appendChild(description_paragraph);
-    return show_container;
-}
-
-
-/*  
-THIS FUNCTION CREATES THE DOM ELEMENT FOR A DETAILED SHOW 
-WHICH WILL BE APPENDED INTO THE SHOW_EXPANDED_MODAL ELEMENT
+THIS FUNCTION CLEARS THE FORMATTING OF THE PAGE BASED ON THREE PARAMETERS
  */
-function createDetailedShowDomElement(detailed_show){    
-    function get_stars(){
-        //Round the rating to the nearest integer
-        let rating = Math.round(detailed_show.rating);        
-        //Based on the rating return x number of stars        
-        let stars_text = '';
-        for (let x = 0; x < rating; x++){            
-            stars_text += '*'
-        }                    
-        return stars_text;
-    }
-    
-    //Container
-    let d_show_container = document.createElement('div');
-    d_show_container.classList.add('detailed_show');
-
-    //Show title
-    let d_show_title_heading = document.createElement('h3');
-    d_show_title_heading.innerText = detailed_show.title;
-    d_show_title_heading.classList.add('d_show_title');
-
-    //Show review in stars
-    let stars_container = document.createElement('span');    
-    stars_container.appendChild(document.createTextNode(get_stars()));    
-
-    //Show image
-    let d_show_image = document.createElement('img');
-    if(detailed_show.image != null){        
-        d_show_image.setAttribute('src', detailed_show.image);
-    }
-    else{
-        d_show_image.setAttribute('alt', 'No image available for this show');
-    }
-
-    //Show Description
-    let d_show_description = document.createElement('p');
-    d_show_description.innerHTML = detailed_show.description;
-
-    //Show Genres
-    let d_show_genres = document.createElement('p');
-    d_show_genres.appendChild(document.createTextNode(detailed_show.genres));
-
-    //Premiered and End dates
-    let d_show_pe = document.createElement('p');
-    d_show_pe.append(document.createTextNode(detailed_show.premiered_date), document.createTextNode(detailed_show.ended_date));
-
-    //Country
-    let d_show_country = document.createElement('p');
-    d_show_country.append(document.createTextNode(detailed_show.country));
-
-    //Official Website
-    let d_show_website = document.createElement('p');
-    d_show_website.append(document.createTextNode(detailed_show.official_site));
-
-    //Put Everything together
-    d_show_container.append(d_show_title_heading, stars_container, d_show_image, d_show_description, d_show_genres, d_show_pe, d_show_country, d_show_website);            
-    return (d_show_container);
-}
-
-
-
-function clearFormatting(search_bar, result_section, show_expanded, search_query){
-    if(search_bar){
-        console.log('Clearing Bar')
+function clearFormatting(search_bar, result_section, search_query){
+    //If this parameter is true, the search bar's text will be cleared
+    if(search_bar){        
         document.getElementById('search_text').value = '';
     }
-    if(search_query){
-        console.log('Clearning Search query Span')
+    //If this parameter is true, the 'showing results...' paragraph will be cleared
+    if(search_query){       
         document.getElementById('search_query').value = '';
     }
-    if(result_section){
-        console.log('Clearing Results')
-        let container = document.getElementById('shows_container')
-        container.innerText = '';
-    }
-    if(show_expanded){
-        console.log('Clearing Show Expanded')
-        let container = document.getElementById('show_expanded_modal')
-        container.innerText = '';
-        container.classList.add('hidden'); 
-        document.getElementById("result_section").classList.remove('opaque');
+    //If this parameter is true, the results section where all the shows are listed will be cleared
+    if(result_section){        
+        document.getElementById('shows_container').innerText = '';        
     }
 }
 
@@ -296,7 +268,7 @@ function clearFormatting(search_bar, result_section, show_expanded, search_query
 
 window.addEventListener("load", function(){
     //Resets the formatting
-    clearFormatting(true, true, true, true);
+    clearFormatting(true, true, true);
 
     //Attach the search function to the search buttons
     document.getElementById("search_start").addEventListener("click", searchShows)
@@ -305,9 +277,4 @@ window.addEventListener("load", function(){
             searchShows();
         }
     })
-
-    //If the user is viewing a detailed show and clicks outside the panel, the panel shuld minimize
-    document.getElementsByTagName('body')[0].addEventListener('click', function(event){
-        clearFormatting(false, false, true, false);
-    });
 })
